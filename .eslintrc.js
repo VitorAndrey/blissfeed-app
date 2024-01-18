09d913d49eb4c5fe3b8b695b8e8fed7613b549b6
Vitor Andrey <vitor.andreylopes@gmail.com>
2024-01-18 20:29:31 -0300

    chore: configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,35 @@ module.exports = {
     '@react-native',
     'plugin:@tanstack/eslint-plugin-query/recommended',
   ],
+  plugins: ['@tanstack/query', 'simple-import-sort'],
+  rules: {
+    '@typescript-eslint/no-var-requires': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'simple-import-sort/imports': [
+      'warning',
+      {
+        groups: [
+          ['^react', '^react-native'],
+          ['^\\u0000$'],
+          ['^@?\\w'],
+          ['^\\u0000$'],
+          [
+            '^@screens',
+            '^@routes',
+            '^@assets',
+            '^@models',
+            '^@contexts',
+            '^@services',
+          ],
+          ['^\\u0000$'],
+          ['^lucide-react-native'],
+          ['^\\u0000$'],
+          ['^@theme'],
+          ['^\\u0000$'],
+          ['^@components'],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'warning',
+  },
 };
